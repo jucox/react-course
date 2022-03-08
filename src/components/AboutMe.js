820fdb53763b054407ce1506d81320aa2138bd80
Renato Junior <renatojr.dev@gmail.com>
2022-03-07 23:20:48 -0300

    aplicando estilos

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,8 @@
+import styles from './AboutMe.module.css'
+
 function AboutMe({name, age, city}) {
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Hello, {name}</h1>
             <p>You are {age} years old and you live in {city}.</p>
         </div>
